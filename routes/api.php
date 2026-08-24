f68459ce4e570025774eddeffc318b784d9450da
Liberu Automation <opensource@liberu.co.uk>
2026-08-24 16:24:56 +0000

    Publish Automation package

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Liberu\Modules\Automation\Voice\Api\Http\Controllers\VoiceResourceController;
 
-Route::middleware(['api', 'auth:sanctum'])->prefix('api/v1/automation/voice')->group(function (): void {
+Route::middleware(['api', 'auth:sanctum', 'throttle:60,1'])->prefix('api/v1/automation/voice')->group(function (): void {
     Route::get('/', [VoiceResourceController::class, 'index']);
     Route::post('/', [VoiceResourceController::class, 'store']);
     Route::get('/{id}', [VoiceResourceController::class, 'show']);
